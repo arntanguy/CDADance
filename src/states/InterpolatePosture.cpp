34f5cd4c19b14f331a4f94fd4fab421b31cba008
Arnaud TANGUY <arn.tanguy@gmail.com>
2023-09-06 17:51:54 +0200

    Add salutation motion

---
+++ src/states/InterpolatePosture.cpp
@@ -0,0 +1,330 @@
+#include "InterpolatePosture.h"
+#include <mc_control/fsm/Controller.h>
+
+#include <iostream>
+#include <random>
+#include <algorithm>
+#include <mc_rtc/io_utils.h>
+
+void InterpolatePosture::start(mc_control::fsm::Controller & ctl)
+{
+  robotName_ = config_("robot", ctl.robot().name());
+  if(!ctl.hasRobot(robotName_))
+  {
+    mc_rtc::log::error_and_throw("[{}] No robot named {}", name(), robotName_);
+  }
+  auto & robot = ctl.robot(robotName_);
+  if(!config_.has(robot.name()))
+  {
+    mc_rtc::log::error_and_throw("[{}] No configuration for robot {}", name(), robot.name());
+  }
+  auto robotConfig = config_(robot.name());
+
+  // Parse the desired posture sequence.
+  // We expect a vector of PostureConfig
+  // Example in yaml:
+  //  posture_sequence:
+  //    - time: 0.0
+  //      posture:
+  //        R_SHOULDER_P: -0.25
+  //        L_SHOULDER_P: -0.21
+  //        R_ELBOW_P: -0.55
+  //        L_ELBOW_P: -0.20
+  //    - time: 2.0
+  //      posture:
+  //        L_SHOULDER_R: 1.38
+  //        L_SHOULDER_Y: 1.22
+  //      shake:
+  //        NECK_Y:
+  //          period: 0.1 # s
+  //          amplitude: 1 # rad
+  config_("autoplay", autoplay_);
+  config_("repeat", repeat_);
+  config_("goBackToInitialPosture", goBackToInitialPosture_);
+  config_("usePostureTransitionCriteria", usePostureTransitionCriteria_);
+  config_("postureTransitionSpeed", postureTransitionSpeed_);
+  config_("enableShake", enableShake_);
+  config_("enableLookAt", enableLookAt_);
+  config_("improvise", improvise_);
+  robotConfig("autoplay", autoplay_);
+  robotConfig("improvise", improvise_);
+  robotConfig("goBackToInitialPosture", goBackToInitialPosture_);
+  robotConfig("usePostureTransitionCriteria", usePostureTransitionCriteria_);
+  robotConfig("postureTransitionSpeed", postureTransitionSpeed_);
+  robotConfig("enableShake", enableShake_);
+  robotConfig("enableLookAt", enableLookAt_);
+  if(ctl.datastore().has("Improvise"))
+  {
+    improvise_ = ctl.datastore().get<bool>("Improvise");
+    ctl.datastore().remove("Improvise");
+  }
+  robotConfig("repeat", repeat_);
+
+  std::vector<PostureConfig> postureSequence = robotConfig("posture_sequence");
+
+  // If improvising, shuffle order
+  if(improvise_)
+  {
+    std::random_device rd;
+    std::mt19937 g{rd()};
+    std::shuffle( postureSequence.begin(), postureSequence.end(), g);
+  }
+
+  // Get the list of actuated joints
+  const auto & rjo = robot.refJointOrder();
+
+  // Start interpolation from current posture
+  PostureConfig initPosture;
+  initPosture.t = 0.0;
+  for(int i = 0; i < rjo.size(); ++i)
+  {
+    std::cout << "Joint " << rjo[i] << std::endl;
+    if(robot.hasJoint(rjo[i]))
+    {
+      initPosture.posture[rjo[i]] = robot.mbc().q[robot.jointIndexInMBC(i)][0];
+    }
+  }
+  postureSequence_.push_back(initPosture);
+
+  // Create a vector used to store the desired value for each actuated joint
+  Eigen::VectorXd desiredPosture(rjo.size());
+  // Initialize with current robot posture
+  for(int i = 0; i < rjo.size(); ++i)
+  {
+    desiredPosture(i) = robot.mbc().q[robot.jointIndexInMBC(i)][0];
+  }
+
+  // Convert to absolute time
+  double t = 0;
+  double scaleTime = robotConfig("scaleTime", 1.);
+  for(auto postureConfig : postureSequence)
+  {
+    t += postureConfig.t * scaleTime;;
+    postureConfig.t = t;
+    postureSequence_.push_back(postureConfig);
+  }
+
+  // Last posture should be the init posture no matter what
+  if(goBackToInitialPosture_)
+  {
+    initPosture.t = postureSequence_.back().t + 2.0;
+    postureSequence_.push_back(initPosture);
+  }
+
+  // Create the interpolator values
+  PostureInterpolator::TimedValueVector interpolatorValues;
+  interpolatorValues.emplace_back(0.0, desiredPosture);
+
+  CoMInterpolator::TimedValueVector comInterpolatorValues;
+  comInterpolatorValues.emplace_back(0.0, Eigen::Vector3d::Zero());
+  // for(const auto & p : postureSequence_)
+  // {
+  //   mc_rtc::log::info("Posture {}", p.save().dump(true));
+  // }
+
+  // For each timed posture in the sequence
+  for(const auto & postureConfig : postureSequence_)
+  {
+    const auto & postureMap = postureConfig.posture;
+    // For each actuated joint
+    for(int i = 0; i < rjo.size(); ++i)
+    {
+      const auto & actuatedJoint = rjo[i];
+      if(!robot.hasJoint(actuatedJoint)) continue;
+      // Check if we have a desired posture in the configuration
+      if(postureMap.count(actuatedJoint))
+      {
+        // If so, put the desired joint value for this actuated joint
+        desiredPosture(i) = postureMap.at(actuatedJoint);
+      }
+      else
+      {
+        // Otherwise use the current joint value
+        desiredPosture(i) = robot.mbc().q[robot.jointIndexInMBC(i)][0];
+      }
+    }
+    // Add the current posture to the interpolator values
+    interpolatorValues.emplace_back(postureConfig.t, desiredPosture);
+    comInterpolatorValues.emplace_back(postureConfig.t, postureConfig.comOffset);
+  }                                                     
+  // Put all desired postures in the interpolator
+  interpolator_.values(interpolatorValues);
+  comInterpolator_.values(comInterpolatorValues);
+
+  // Load the configuration for the posture task.
+  // See https://jrl-umi3218.github.io/mc_rtc/json.html#MetaTask/PostureTask for
+  // supported values
+  // Example in yaml:
+  //   posture_task:
+  //     stiffness: 100
+  auto & postureTask = *ctl.getPostureTask(robot.name());
+  postureTask.load(ctl.solver(), robotConfig("posture_task", mc_rtc::Configuration{}));
+
+  lookAt_ = std::make_shared<mc_tasks::LookAtTask>(ctl.robot().frame("NECK_P_LINK"), Eigen::Vector3d{1,0,0}, 10.0, 100.0);
+
+  ctl.gui()->addElement(
+      this, {name()},
+      mc_rtc::gui::Checkbox(
+          "Play", [this]() { return autoplay_; }, [this]() { autoplay_ = !autoplay_; }),
+      mc_rtc::gui::Checkbox(
+          "Update posture", [this]() { return updatePosture_; }, [this]() { updatePosture_ = !updatePosture_; }),
+      mc_rtc::gui::NumberInput(
+          "Time", [this]() { return t_; }, [this](double t) { t_ = t; }),
+      mc_rtc::gui::NumberSlider(
+          "Time selector", [this]() { return t_; }, [this](double t) { t_ = t; }, 0,
+          interpolator_.values().back().first),
+      mc_rtc::gui::Checkbox("Repeat Motion", [this]() { return repeat_; },
+                            [this]() { repeat_ = !repeat_; }),
+      mc_rtc::gui::Checkbox("Improvise next time",
+                            [this, &ctl]()
+                            {
+                              return ctl.datastore().has("Improvise") && ctl.datastore().get<bool>("Improvise");
+                            },
+                            [this, &ctl]()
+                            {
+                              if(!ctl.datastore().has("Improvise"))
+                              {
+                                ctl.datastore().make<bool>("Improvise", true);
+                              }
+                              else
+                              {
+                                ctl.datastore().remove("Improvise");
+                              }
+                            }),
+      mc_rtc::gui::Checkbox("Improvising?", [this]() { return improvise_; },
+                            [this]() {}),
+      mc_rtc::gui::Checkbox("Enable Shake", [this]() { return enableShake_; },
+                            [this]() { enableShake_ = !enableShake_; }),
+      mc_rtc::gui::Checkbox("Enable LookAt", [this]() { return enableLookAt_; },
+                            [this]() { enableLookAt_ = !enableLookAt_; })
+      );
+
+}
+
+bool InterpolatePosture::run(mc_control::fsm::Controller & ctl)
+{
+  auto & robot = ctl.robot(robotName_);
+  const auto & rjo = robot.refJointOrder();
+
+  // Compute the interpolated posture at the current time
+  auto desiredPosture = interpolator_.compute(t_);
+  auto desiredCoMOffset = comInterpolator_.compute(t_);
+
+  // Shake
+  auto currPostureSeq =
+      std::find_if(postureSequence_.begin(), postureSequence_.end(), [this](const auto & p) { return p.t > t_; });
+  currPostureSeq--;
+  if(currPostureSeq != postureSequence_.end())
+  {
+    if(enableShake_)
+    {
+      const auto & shakeMap = currPostureSeq->shake;
+      // mc_rtc::log::info("Should shake (t={}, posture t= {})", t_, currPostureSeq->t);
+      // mc_rtc::log::info("Joints: {}", mc_rtc::io::to_string(shakeMap, [](const auto & m) { return m.first; }));
+      // For each actuated joint
+      for(int i = 0; i < rjo.size(); ++i)
+      {
+        // Shake
+        const auto & actuatedJoint = rjo[i];
+        if(shakeMap.count(actuatedJoint))
+        {
+          const auto & shakeConfig = shakeMap.at(actuatedJoint);
+          // Shake value such that it starts with
+          // - Shake = 0 for t_ = currPostureSeq->t (no motion initially)
+          // - It shakes with period shakeConfig.period around the current joint
+          // trajectory value
+          // - It shakes with amplitude shakeConfig.amplitude
+          double shakeVal =
+              shakeConfig.amplitude * sin(2 * mc_rtc::constants::PI / shakeConfig.period * (t_ - currPostureSeq->t));
+          desiredPosture(i) += shakeVal;
+          // mc_rtc::log::info("Shaking joint {} : {}", actuatedJoint, shakeVal);
+        }
+      }
+    }
+
+    // LookAt task
+    if(enableLookAt_)
+    {
+      const auto & lookAtConfig = currPostureSeq->lookAt;
+      if(lookAtConfig)
+      {
+        auto lookRobot = lookAtConfig->robot ? * lookAtConfig->robot : ctl.robot().name();
+        lookAt_->target(ctl.robot(lookRobot).frame(lookAtConfig->frame).position().translation());
+        if(!lookAtActive_)
+        {
+          lookAt_->stiffness(lookAtConfig->stiffness);
+          lookAt_->weight(lookAtConfig->weight);
+          ctl.solver().addTask(lookAt_);
+          lookAtActive_ = true;
+        }
+      }
+      else
+      {
+        if(lookAtActive_)
+        {
+          ctl.solver().removeTask(lookAt_);
+          lookAtActive_ = false;
+        }
+      }
+    }
+  }
+
+  // Get the posture task
+  auto & postureTask = *ctl.getPostureTask(robot.name());
+  // Copy the current posture target
+  auto posture = postureTask.posture();
+
+  // For each actuated joint
+  for(int i = 0; i < rjo.size(); ++i)
+  {
+    const auto & actuatedJoint = rjo[i];
+    // Set the posture target for this actuated joint to its interpolated value
+    posture[robot.jointIndexInMBC(i)][0] = desiredPosture[i];
+  }
+
+  // Change the posture target in the posture task
+  if(updatePosture_)
+  {
+    postureTask.posture(posture);
+  }
+
+  if(updateCoM_)
+  {
+    if(ctl.datastore().has("StabilizerStandingState::setCoMTarget"))
+    {
+      auto comTarget = ctl.datastore().call<const Eigen::Vector3d &>("StabilizerStandingState::getCoMTarget");
+      comTarget.z() = robot.module()._lipmStabilizerConfig.comHeight + desiredCoMOffset.z(); 
+      ctl.datastore().call<void>("StabilizerStandingState::setCoMTarget",
+          static_cast<const Eigen::Vector3d &>(comTarget));
+    }
+  }
+
+  if(autoplay_)
+  {
+    t_ += ctl.timeStep;
+  }
+
+  bool finished = (t_ >= interpolator_.values().back().first)
+                && (!usePostureTransitionCriteria_ || postureTask.speed().norm() < postureTransitionSpeed_);
+
+  if(repeat_)
+  {
+    output("Repeat");
+  }
+  else
+  {
+    output("Stop");
+  }
+  return finished;
+}
+
+void InterpolatePosture::teardown(mc_control::fsm::Controller & ctl)
+{
+  ctl.gui()->removeCategory({name()});
+  if(lookAtActive_)
+  {
+    ctl.solver().removeTask(lookAt_);
+  }
+}
+
+EXPORT_SINGLE_STATE("InterpolatePosture", InterpolatePosture)
---
+++ src/states/InterpolatePosture.h
@@ -0,0 +1,201 @@
+#pragma once
+
+#include <mc_control/fsm/State.h>
+#include <mc_trajectory/SequenceInterpolator.h>
+#include <Eigen/Core>
+#include <mc_tasks/LookAtFrameTask.h>
+
+/**
+ * Configuration for the shaking motion (per-joint)
+ */
+struct Shake
+{
+  double period = 1;
+  double amplitude = 1.0;
+
+  void load(const mc_rtc::Configuration & config)
+  {
+    period = config("period");
+    amplitude = config("amplitude");
+  }
+
+  mc_rtc::Configuration save() const
+  {
+    mc_rtc::Configuration c;
+    c.add("period", period);
+    c.add("amplitude", amplitude);
+    return c;
+  }
+};
+
+namespace mc_rtc
+{
+template<>
+struct ConfigurationLoader<Shake>
+{
+  static Shake load(const mc_rtc::Configuration & config)
+  {
+    Shake shake;
+    shake.load(config);
+    return shake;
+  }
+
+  static mc_rtc::Configuration save(const Shake & object)
+  {
+    return object.save();
+  }
+};
+} // namespace mc_rtc
+
+/**
+ * Configuration for the shaking motion (per-joint)
+ */
+struct LookAtConfig
+{
+  std::string frame{""};
+  std::optional<std::string> robot{std::nullopt};
+  double stiffness = 20;
+  double weight = 1000;
+
+  void load(const mc_rtc::Configuration & config)
+  {
+    frame = static_cast<std::string>(config("frame"));
+    if(config.has("robot"))
+    {
+      robot = config("robot");
+    }
+    config("stiffness", stiffness);
+    config("weight", weight);
+  }
+
+  mc_rtc::Configuration save() const
+  {
+    mc_rtc::Configuration c;
+    c.add("frame", frame);
+    if(robot)
+    {
+      c.add("robot", *robot);
+    }
+    c.add("stiffness", stiffness);
+    c.add("weight", weight);
+    return c;
+  }
+};
+
+namespace mc_rtc
+{
+template<>
+struct ConfigurationLoader<LookAtConfig>
+{
+  static LookAtConfig load(const mc_rtc::Configuration & config)
+  {
+    LookAtConfig lookat;
+    lookat.load(config);
+    return lookat;
+  }
+
+  static mc_rtc::Configuration save(const LookAtConfig & object)
+  {
+    return object.save();
+  }
+};
+} // namespace mc_rtc
+
+/**
+ * Configuration for a posture
+ */
+struct PostureConfig
+{
+  double t;
+  std::map<std::string, double> posture;
+  std::map<std::string, Shake> shake;
+  Eigen::Vector3d comOffset = Eigen::Vector3d::Zero();
+  std::optional<LookAtConfig> lookAt{std::nullopt};
+
+  void load(const mc_rtc::Configuration & config)
+  {
+    t = config("time");
+    posture = config("posture");
+    if(config.has("shake"))
+    {
+      shake = config("shake");
+    }
+    if(config.has("lookAt"))
+    {
+      lookAt = config("lookAt");
+    }
+    else
+    {
+      lookAt = std::nullopt;
+    }
+    config("comOffset", comOffset);
+  }
+
+  mc_rtc::Configuration save() const
+  {
+    mc_rtc::Configuration c;
+    c.add("time", t);
+    c.add("posture", posture);
+    c.add("shake", shake);
+    if(lookAt)
+    {
+      c.add("lookAt", *lookAt);
+    }
+    c.add("comOffset", comOffset);
+    return c;
+  }
+};
+
+namespace mc_rtc
+{
+template<>
+struct ConfigurationLoader<PostureConfig>
+{
+  static PostureConfig load(const mc_rtc::Configuration & config)
+  {
+    PostureConfig shake;
+    shake.load(config);
+    return shake;
+  }
+
+  static mc_rtc::Configuration save(const PostureConfig & object)
+  {
+    return object.save();
+  }
+};
+} // namespace mc_rtc
+
+struct InterpolatePosture : mc_control::fsm::State
+{
+
+  void start(mc_control::fsm::Controller & ctl) override;
+
+  bool run(mc_control::fsm::Controller & ctl) override;
+
+  void teardown(mc_control::fsm::Controller & ctl) override;
+
+private:
+  using PostureInterpolator = mc_trajectory::SequenceInterpolator<Eigen::VectorXd>;
+  using CoMInterpolator = mc_trajectory::SequenceInterpolator<Eigen::Vector3d>;
+  PostureInterpolator interpolator_;
+  CoMInterpolator comInterpolator_;
+  double t_{0};
+  bool autoplay_ = true;
+  bool updatePosture_ = true;
+  bool updateCoM_ = true;
+  bool improvise_ = false;
+  bool repeat_ = false;
+  bool goBackToInitialPosture_ = true;
+  bool enableShake_ = true;
+  std::string robotName_;
+
+  bool usePostureTransitionCriteria_ = false;
+  double postureTransitionSpeed_ = 1e-10;
+
+  std::vector<PostureConfig> postureSequence_;
+
+  bool enableLookAt_ = true;
+  std::shared_ptr<mc_tasks::LookAtTask> lookAt_;
+  bool lookAtActive_ = false;
+  std::string lookAtFrame_{};
+};
